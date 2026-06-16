12ac904f280b0de327717f31eb4e9730da8e696d
shifin shalu <shifinshalu035@gmail.com>
2026-06-16 22:03:38 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Categories from "./component/catogories";
+import Footer from "./component/footer";
+import Hero from "./component/Hero";
+function App(){
+  return(
+   <>
+      
+      <Hero/>
+      <Categories/>  
+      <Footer/>
+  </>
+  );
+}
+export default App;
